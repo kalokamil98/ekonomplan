d814fad27902ed3cad1288fd3470528629d89ea6
kalokamil98 <73557117+kalokamil98@users.noreply.github.com>
2021-04-07 09:58:43 +0200

    godzina fix

--- lekcje.py
+++ lekcje.py
@@ -36,7 +36,7 @@ def wyslanie(x):
 def wyslanie2(x):
     x
     
-    hook.send(f'{d.hour+1}godzina,{d.minute} minuta ,{lekcja[x]} <@&803931239617789962>')
+    hook.send(f'{d.hour+2}godzina,{d.minute} minuta ,{lekcja[x]} <@&803931239617789962>')
     time.sleep(60)
 
 
@@ -47,32 +47,32 @@ def wyslanie2(x):
 #wotek i dzialania sprwdznie godziny
 
 def poniedzialek():
-    if d.hour == 7 and d.minute == 0:
+    if d.hour == 7-1 and d.minute == 0:
         wyslanie(4)
         wyslanie2(7)
-    elif d.hour == 7 and d.minute == 55:
+    elif d.hour == 7-1 and d.minute == 55:
         wyslanie(7)
         wyslanie2(4)
-    elif d.hour == 8 and d.minute == 45:
+    elif d.hour == 8-1 and d.minute == 45:
         wyslanie(11)
         wyslanie2(3)
 
-    elif d.hour == 9 and d.minute ==50:
+    elif d.hour == 9-1 and d.minute ==50:
         wyslanie(9)
         wyslanie2(12)
 
-    elif d.hour == 10 and d.minute ==45:
+    elif d.hour == 10-1 and d.minute ==45:
         wyslanie(0)
         wyslanie2(0)
 
-    elif d.hour == 11 and d.minute ==35:
+    elif d.hour == 11-1 and d.minute ==35:
         wyslanie(3)
         wyslanie2(7)
 
-    elif d.hour == 12 and d.minute ==25:
+    elif d.hour == 12-1 and d.minute ==25:
         wyslanie(9)
         wyslanie2(3)
-    elif d.hour == 13 and d.minute ==15:
+    elif d.hour == 13-1 and d.minute ==15:
         wyslanie(5)
         wyslanie2(5)
     
@@ -81,95 +81,95 @@ def poniedzialek():
 
 
 def wtorek():
-    if d.hour == 7 and d.minute == 0:
+    if d.hour == 7-1 and d.minute == 0:
         wyslanie(1)
         wyslanie2(1)
-    elif d.hour == 7 and d.minute == 55:
+    elif d.hour == 7-1 and d.minute == 55:
         wyslanie(1)
         wyslanie2(1)
-    elif d.hour == 8 and d.minute == 45:
+    elif d.hour == 8-1 and d.minute == 45:
         wyslanie(13)
         wyslanie2(13)
 
-    elif d.hour == 9 and d.minute ==50:
+    elif d.hour == 9-1 and d.minute ==50:
         wyslanie(8)
         wyslanie2(9)
 
-    elif d.hour == 10 and d.minute ==45:
+    elif d.hour == 10-1 and d.minute ==45:
         wyslanie(8)
         wyslanie2(9)
-    elif d.hour == 11 and d.minute ==35:
+    elif d.hour == 11-1 and d.minute ==35:
         wyslanie(7)
         wyslanie2(11)
-    elif d.hour == 12 and d.minute ==25:
+    elif d.hour == 12-1 and d.minute ==25:
         wyslanie(3)
         wyslanie2(11)
-    elif d.hour == 13 and d.minute ==15:
+    elif d.hour == 13-1 and d.minute ==15:
         wyslanie(5)
         wyslanie2(5)
 
 
 def sroda():
-    if d.hour == 7 and d.minute == 0:
+    if d.hour == 7-1 and d.minute == 0:
         wyslanie(11)
         time.sleep(60)
-    elif d.hour == 7 and d.minute == 55:
+    elif d.hour == 7-1 and d.minute == 55:
         wyslanie(6)
         wyslanie2(6)
 
-    elif d.hour == 9 and d.minute ==50:
+    elif d.hour == 9-1 and d.minute ==50:
         wyslanie(6)
         wyslanie2(6)
 
-    elif d.hour == 10 and d.minute ==45:
+    elif d.hour == 10-1 and d.minute ==45:
         wyslanie(14)
         wyslanie2(14)
 
-    elif d.hour == 11 and d.minute ==35:
+    elif d.hour == 11-1 and d.minute ==35:
         wyslanie(5)
         wyslanie2(5)
 
-    elif d.hour == 12 and d.minute ==25:
+    elif d.hour == 12-1 and d.minute ==25:
         wyslanie(3)
         wyslanie2(8)
-    elif d.hour == 13 and d.minute ==15:
+    elif d.hour == 13-1 and d.minute ==15:
         wyslanie(3)
         time.sleep(60)
 
 def czwartek():
-    if d.hour == 7 and d.minute == 0:
+    if d.hour == 7-1 and d.minute == 0:
         wyslanie(10)
         wyslanie2(10)
-    elif d.hour == 7 and d.minute == 55:
+    elif d.hour == 7-1 and d.minute == 55:
         wyslanie(12)
         wyslanie2(10)
-    elif d.hour == 8 and d.minute == 45:
+    elif d.hour == 8-1 and d.minute == 45:
         wyslanie(14)
         wyslanie2(14)
 
 def piatek():
-    if d.hour == 7 and d.minute == 55:
+    if d.hour == 7-1 and d.minute == 55:
         wyslanie2(8)
-    elif d.hour == 8 and d.minute == 45:
+    elif d.hour == 8-1 and d.minute == 45:
         wyslanie(6)
         wyslanie2(6)
 
-    elif d.hour == 9 and d.minute ==50:
+    elif d.hour == 9-1 and d.minute ==50:
         wyslanie(2)
         wyslanie2(2)
 
-    elif d.hour == 10 and d.minute ==45:
+    elif d.hour == 10-1 and d.minute ==45:
         wyslanie(16)
         wyslanie2(16)
 
-    elif d.hour == 11 and d.minute ==35:
+    elif d.hour == 11-1 and d.minute ==35:
         wyslanie(10)
         wyslanie2(3)
 
-    elif d.hour == 12 and d.minute ==25:
+    elif d.hour == 12-1 and d.minute ==25:
         wyslanie(6)
         wyslanie2(6)
-    elif d.hour == 13 and d.minute ==15:
+    elif d.hour == 13-1 and d.minute ==15:
         wyslanie2(3)
  
 
